f8c99409cad8c0570b88353d232ae5fbfb2ac1ed
Hannah Shore <shore.hannah.e@gmail.com>
2020-07-02 21:49:13 -0400

    <3

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,8 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h2>Hi, Brad. I love you <3</h2>
+
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
