9bdc7b4f9117236e099ab9a00479041bc94ea484
Hannah Shore <hannah@primary.com>
2020-07-09 19:40:11 -0400

    Add jest.

---
+++ pages/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Home from './index';
+
+describe('index page', () => {
+  it('should have Head component', () => {
+    const subject = mount(<Home />);
+
+    expect(subject.find('Head')).toHaveLength(1);
+  });
+});
