92250e0dd24d81a4e4c8af9eb6f4ac6ae037d40f
Hannah Shore <shore.hannah.e@gmail.com>
2020-07-11 22:04:43 -0400

    Basic links (#9)

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import "./index.css"
+import PropTypes from "prop-types"
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object,
+}
+
+export default MyApp
