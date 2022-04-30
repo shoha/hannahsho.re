3f94c975e4b129873e833ef21c6f2bb4768d83b2
Hannah Shore <lookuphannahlookup@gmail.com>
2022-04-30 19:27:18 -0400

    POST API (#24)
    * Call hdcli
    
    * Lint
    
    * CORS
    
    * POST API
    
    * lint

--- pages/api/hd.js
+++ pages/api/hd.js
@@ -1,4 +1,5 @@
 import { exec } from "child_process"
+import * as fs from "fs/promises"
 import NextCors from "nextjs-cors"
 import { promisify } from "util"
 
@@ -27,23 +28,37 @@ export default async (req, res) => {
   }
 }
 
-const get = async (_, res) => {
-  const results = await execPr(
-    "./java/jdk-18.0.1/bin/java -jar hdcli.jar public/input.hdc public/format.hde public/output.xml"
-  )
-
-  res.statusCode = 200
-  res.json(results)
-  // const results = await execPr("./java/jdk-18.0.1/bin/java -version")
-  // res.statusCode = 200
-  // res.json(results)
+const get = async (req, res) => {
+  await post(req, res)
 }
 
-const post = async (_, res) => {
-  const results = await execPr(
-    "./java/jdk-18.0.1/bin/java -jar hdcli.jar public/input.hdc public/format.hde public/output.xml"
+const post = async (req, res) => {
+  let inputName = "public/input.hdc"
+  let inputFile
+
+  if (req.body) {
+    inputName = `tmp/in_${Date.now()}_${Math.ceil(
+      Math.random() * 1000000
+    ).toString()}`
+    inputFile = fs.writeFile(inputName, req.body, { encoding: "utf16le" })
+  }
+
+  const outputName = `tmp/out_${Date.now()}_${Math.ceil(
+    Math.random() * 1000000
+  ).toString()}`
+
+  await execPr(
+    `./java/jdk-18.0.1/bin/java -jar hdcli.jar ${inputName} public/format.hde ${outputName}`
   )
 
+  const exportBuffer = await fs.readFile(outputName)
+
   res.statusCode = 200
-  res.json(results)
+  res.json({ body: exportBuffer.toString() })
+
+  if (inputFile) {
+    await fs.unlink(inputName)
+  }
+
+  await fs.unlink(outputName)
 }
