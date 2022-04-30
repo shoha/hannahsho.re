abeda28bb2b2626680f7ae198bf9024a73096e6e
Hannah Shore <lookuphannahlookup@gmail.com>
2022-04-30 17:14:37 -0400

    Call hdcli (#23)
    * Call hdcli
    
    * Lint
    
    * CORS

---
+++ pages/api/hd.js
@@ -0,0 +1,49 @@
+import { exec } from "child_process"
+import NextCors from "nextjs-cors"
+import { promisify } from "util"
+
+const execPr = promisify(exec)
+
+export default async (req, res) => {
+  const { method } = req
+
+  await NextCors(req, res, {
+    methods: ["GET", "POST"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+
+  switch (method) {
+    case "GET":
+      await get(req, res)
+      break
+    case "POST":
+      await post(req, res)
+      break
+    default:
+      res.statusCode = 404
+      res.json({ error: "Method not supported" })
+      break
+  }
+}
+
+const get = async (_, res) => {
+  const results = await execPr(
+    "./java/jdk-18.0.1/bin/java -jar hdcli.jar public/input.hdc public/format.hde public/output.xml"
+  )
+
+  res.statusCode = 200
+  res.json(results)
+  // const results = await execPr("./java/jdk-18.0.1/bin/java -version")
+  // res.statusCode = 200
+  // res.json(results)
+}
+
+const post = async (_, res) => {
+  const results = await execPr(
+    "./java/jdk-18.0.1/bin/java -jar hdcli.jar public/input.hdc public/format.hde public/output.xml"
+  )
+
+  res.statusCode = 200
+  res.json(results)
+}
